0917d63ac7d30cad5fd30a8421bde62b9ec71e00
Mario Andrés Correa <a@macool.me>
2017-05-04 13:03:40 -0500

    store sessions in DB

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170320230236) do
+ActiveRecord::Schema.define(version: 20170504180032) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,15 @@ ActiveRecord::Schema.define(version: 20170320230236) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.index ["lat", "lng"], name: "index_places_on_lat_and_lng", using: :btree
+  end
+
+  create_table "sessions", force: :cascade do |t|
+    t.string   "session_id", null: false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.index ["session_id"], name: "index_sessions_on_session_id", unique: true, using: :btree
+    t.index ["updated_at"], name: "index_sessions_on_updated_at", using: :btree
   end
 
   create_table "songs", force: :cascade do |t|
