3e33bca1379f5d44fca6c8b4df2e859adcfd725a
Mario Andrés Correa <a@macool.me>
2017-05-19 08:13:30 -0500

    endpoint para listar amigows

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,14 @@ class User < ActiveRecord::Base
 
   belongs_to :current_place, optional: true, class_name: "Place"
   has_many :events, dependent: :destroy
+
+  def self.search(q)
+    where("name ILIKE :q", q: "%#{q}%")
+  end
+
+  def friends
+    self.class.where.not(
+      id: id
+    )
+  end
 end
