145fe1c3cb8622b4cad45e80b692ed0aa6841292
Fabricio Flores <fabrifloresg@gmail.com>
2017-05-29 23:03:14 -0500

    get rid of legacy places

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,6 @@ class User < ActiveRecord::Base
           :omniauthable, omniauth_providers: [:facebook]
   include DeviseTokenAuth::Concerns::User
 
-  belongs_to :current_place, optional: true, class_name: "Place"
   has_many :events, dependent: :destroy
 
   def self.search(q)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170524123557) do
+ActiveRecord::Schema.define(version: 20170530035831) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,16 +36,6 @@ ActiveRecord::Schema.define(version: 20170524123557) do
     t.index ["user_id"], name: "index_invitees_on_user_id", using: :btree
   end
 
-  create_table "places", force: :cascade do |t|
-    t.string   "name",        null: false
-    t.string   "picture_url"
-    t.float    "lat",         null: false
-    t.float    "lng",         null: false
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.index ["lat", "lng"], name: "index_places_on_lat_and_lng", using: :btree
-  end
-
   create_table "sessions", force: :cascade do |t|
     t.string   "session_id", null: false
     t.text     "data"
@@ -56,7 +46,6 @@ ActiveRecord::Schema.define(version: 20170524123557) do
   end
 
   create_table "songs", force: :cascade do |t|
-    t.integer  "place_id",                       null: false
     t.integer  "user_id",                        null: false
     t.string   "spotify_id",                     null: false
     t.boolean  "now_playing",    default: false
@@ -65,7 +54,6 @@ ActiveRecord::Schema.define(version: 20170524123557) do
     t.datetime "updated_at",                     null: false
     t.json     "spotify_track"
     t.index ["already_played"], name: "index_songs_on_already_played", using: :btree
-    t.index ["place_id"], name: "index_songs_on_place_id", using: :btree
     t.index ["user_id"], name: "index_songs_on_user_id", using: :btree
   end
 
@@ -88,8 +76,6 @@ ActiveRecord::Schema.define(version: 20170524123557) do
     t.text     "tokens"
     t.datetime "created_at",                               null: false
     t.datetime "updated_at",                               null: false
-    t.integer  "current_place_id"
-    t.index ["current_place_id"], name: "index_users_on_current_place_id", using: :btree
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true, using: :btree
@@ -98,7 +84,5 @@ ActiveRecord::Schema.define(version: 20170524123557) do
   add_foreign_key "events", "users"
   add_foreign_key "invitees", "events"
   add_foreign_key "invitees", "users"
-  add_foreign_key "songs", "places"
   add_foreign_key "songs", "users"
-  add_foreign_key "users", "places", column: "current_place_id"
 end
